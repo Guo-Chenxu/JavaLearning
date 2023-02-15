23ed7d86da5a31b0cf1c159bf1225368af9c460e
Guo-Chenxu <90923304+Guo-Chenxu@users.noreply.github.com>
2023-02-16 00:45:04 +0800

    Add files via upload

--- com/qq/qqserver/qqframe/QQFrame.java
+++ com/qq/qqserver/qqframe/QQFrame.java
@@ -1,1 +1,14 @@
+package com.qq.qqserver.qqframe;
 
+import com.qq.qqserver.service.QQServer;
+
+import java.io.IOException;
+
+/**
+ * 该类创建QQServer, 启动后台服务
+ */
+public class QQFrame {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        new QQServer();
+    }
+}
